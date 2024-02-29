aab9b49aa81a6eb0d700aa859978494eb3bf319b
DayanaHernandez <dayanahernandezhincapie@gmail.com>
2024-02-28 22:02:21 -0500

    ejercico en clase

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  
+  return (
+    <section className="contenido">
+      <header>
+        <img src="public/vite.svg" alt=""/>
+        <nav>
+          <a href="">Servicios</a>
+          <a href="">Contacto</a>
+          <a href="">Productos</a>
+        </nav>
+      </header>
+      <aside></aside>
+     <main>
+      <section></section>
+      <section></section>
+      </main> 
+      
+    </section>
+  )
+}
+
+export default App
